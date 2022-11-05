4f57a413890d62146f6215676184c5a3a6a5efbc
iivlx <iivlx@iivlx.com>
2022-11-05 14:06:35 -0700

    Add header files for registering native function calls.

---
+++ include/offsets.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// version 1.0.4.0
+
+#define OFF_ZONE 0xDF6316 // WCHAR[64] - current zone
+#define OFF_STREET 0xDF6498 // WCHAR[64] - current street
+#define OFF_CAR 0xDF659c // WCHAR[64] - current car
