06d267bd96fa17f35644d189e31f1afc4a1538d0
iivlx <iivlx@iivlx.com>
2022-11-09 19:51:48 -0800

    Update native functions...
    ADD_BLIP_FOR_COORD
    GET_BLIP_INFO_ID_TYPE
    GET_FIRST_BLIP_INFO_ID

--- include/natives.h
+++ include/natives.h
@@ -2,6 +2,7 @@
 
 // version 1.0.4.0
 
+typedef void(__cdecl* _ADD_BLIP_FOR_COORD)(float x, float y, float z, void* pBlip);
 typedef void(__cdecl* _CHANGE_CAR_COLOUR)(int vehicleId, int color1, int color2);
 typedef void(__cdecl* _CLEAR_WANTED_LEVEL)(int charId);
 typedef int(__cdecl* _CONVERT_INT_TO_PLAYERINDEX)(int);
@@ -11,7 +12,9 @@ typedef void(__cdecl* _DISPLAY_TEXT)(float x, float y, const char* text);
 typedef void(__cdecl* _DISPLAY_TEXT_WITH_NUMBER)(float x, float y, char* text, int number);
 typedef void(__cdecl* _DISPLAY_AREA_NAME)(int display);
 typedef void(__cdecl* _FIND_STREET_NAME_AT_POSITION)(float x, float y, float z, int* street1, int* street2);
+typedef int(__cdecl* _GET_BLIP_INFO_ID_TYPE)(int id);
 typedef void(__cdecl* _GET_CHAR_COORDINATES)(int charId, float* x, float* y, float* z);
+typedef int(__cdecl* _GET_FIRST_BLIP_INFO_ID)(int id);
 typedef void(__cdecl* _GET_PLAYER_CHAR)(int charId, void* pChar);
 typedef int(__cdecl* _GET_PLAYER_ID)(void);
 typedef void(__cdecl* _GIVE_PLAYER_HELMET)(int playerIndex);
@@ -27,6 +30,7 @@ typedef void(__cdecl* _UNPAUSE_GAME)(void);
 
 typedef void(__thiscall* _updateCarColor)(void* pCar);
 
+static _ADD_BLIP_FOR_COORD ADD_BLIP_FOR_COORD;
 static _CHANGE_CAR_COLOUR CHANGE_CAR_COLOUR;
 static _CLEAR_WANTED_LEVEL CLEAR_WANTED_LEVEL;
 static _CONVERT_INT_TO_PLAYERINDEX CONVERT_INT_TO_PLAYERINDEX;
@@ -36,7 +40,9 @@ static _DISPLAY_CASH DISPLAY_CASH;
 static _DISPLAY_TEXT DISPLAY_TEXT;
 static _DISPLAY_TEXT_WITH_NUMBER DISPLAY_TEXT_WITH_NUMBER;
 static _FIND_STREET_NAME_AT_POSITION FIND_STREET_NAME_AT_POSITION;
+static _GET_BLIP_INFO_ID_TYPE GET_BLIP_INFO_ID_TYPE;
 static _GET_CHAR_COORDINATES GET_CHAR_COORDINATES;
+static _GET_FIRST_BLIP_INFO_ID GET_FIRST_BLIP_INFO_ID;
 static _GET_PLAYER_CHAR GET_PLAYER_CHAR;
 static _GET_PLAYER_ID GET_PLAYER_ID;
 static _GET_STRING_FROM_HASH_KEY GET_STRING_FROM_HASH_KEY;
@@ -53,6 +59,7 @@ static _UNPAUSE_GAME UNPAUSE_GAME;
 static _updateCarColor updateCarColor;
 
 void registerFunctions(uintptr_t moduleBase) {
+  ADD_BLIP_FOR_COORD = (_ADD_BLIP_FOR_COORD)(moduleBase + 0x6134B0);
   CHANGE_CAR_COLOUR = (_CHANGE_CAR_COLOUR)(moduleBase + 0x5DEDF0);
   CLEAR_WANTED_LEVEL = (_CLEAR_WANTED_LEVEL)(moduleBase + 0x5F6F60);
   CONVERT_INT_TO_PLAYERINDEX = (_CONVERT_INT_TO_PLAYERINDEX)(moduleBase + 0x5F6140);
@@ -62,7 +69,9 @@ void registerFunctions(uintptr_t moduleBase) {
   DISPLAY_TEXT = (_DISPLAY_TEXT)(moduleBase + 0x611B50);
   DISPLAY_TEXT_WITH_NUMBER = (_DISPLAY_TEXT_WITH_NUMBER)(moduleBase + 0x611EA0);
   FIND_STREET_NAME_AT_POSITION = (_FIND_STREET_NAME_AT_POSITION)(moduleBase + 0x5CE540);
+  GET_BLIP_INFO_ID_TYPE = (_GET_BLIP_INFO_ID_TYPE)(moduleBase + 0x613320);
   GET_CHAR_COORDINATES = (_GET_CHAR_COORDINATES)(moduleBase + 0x5FB910);
+  GET_FIRST_BLIP_INFO_ID = (_GET_FIRST_BLIP_INFO_ID)(moduleBase + 0x613D20);
   GET_PLAYER_CHAR = (_GET_PLAYER_CHAR)(moduleBase + 0x5F7ED0);
   GET_PLAYER_ID = (_GET_PLAYER_ID)(moduleBase + 0x5F6110);
   GET_STRING_FROM_HASH_KEY = (_GET_STRING_FROM_HASH_KEY)(moduleBase + 0x6116D0);
